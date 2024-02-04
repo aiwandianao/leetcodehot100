9cfde12d930ca8c44289a5ea1644b7a44b29c0ee
aiwandianao <237921308@qq.com>
2024-02-04 11:20:47 +0800

    init

---
+++ src/leetcode.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class leetcode {
+
+    public int longestConsecutive(int[] nums) {
+        int ans = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            set.add(i);
+        }
+        for (int i : set) {
+            int cnt = 0;
+            int tmp = i;
+            if (!set.contains(i - 1)) {
+                while (set.contains(tmp)) {
+                    tmp++;
+                    cnt++;
+                }
+                ans = Math.max(cnt, ans);
+            }
+        }
+        return ans;
+    }
+}
+
+
+
